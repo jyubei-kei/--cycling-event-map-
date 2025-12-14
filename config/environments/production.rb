265ae5e7b559fc15c824f367e0491d598aa5b1d9
jyubei-kei <jyubei_shin@outlook.jp>
2025-12-14 10:55:45 +0900

    Fix typo in production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  conrig.action_mailer.default_url_options = {
+  config.action_mailer.default_url_options = {
     host: 'cycling-event-map-jyubei-64ee25812261.herokuapp.com',
     protocol: 'https'
   }
