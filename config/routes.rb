306fc474711c91947c7527436d096a65cce6f103
jyubei-kei <jyubei_shin@outlook.jp>
2025-12-14 12:12:39 +0900

    top-page-improvement

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'mypages/show'
   devise_for :users
   root "home#top"
+  get "/mypage", to: "mypages#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
