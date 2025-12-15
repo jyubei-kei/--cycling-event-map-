d0f08916c9fdb5583ca2fec01a83478f4aa9ba1c
jyubei-kei <jyubei_shin@outlook.jp>
2025-12-15 21:36:22 +0900

    admin_crud

---
+++ app/controllers/admin/events_controller.rb
@@ -0,0 +1,49 @@
+class Admin::EventsController < ApplicationController
+  before_action :set_event, only: %i[show edit update destroy]
+
+  def index
+    @events = Event.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to admin_event_path(@event), notice: "イベントを作成しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to admin_event_path(@event), notice: "イベントを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to admin_events_path, notice: "イベントを削除しました"
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :description, :starts_at, :ends_at)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_12_14_011058) do
+ActiveRecord::Schema[7.0].define(version: 2025_12_15_120047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +33,10 @@ ActiveRecord::Schema[7.0].define(version: 2025_12_14_011058) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
